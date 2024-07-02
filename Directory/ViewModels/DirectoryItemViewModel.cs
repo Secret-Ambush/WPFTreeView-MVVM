7934070137a71a3bed586ef1671643e36127aff7
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-07-02 15:32:58 +0400

    Update DirectoryItemViewModel.cs

--- Directory/ViewModels/DirectoryItemViewModel.cs
+++ Directory/ViewModels/DirectoryItemViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfTreeView;
 
 namespace WPFTreeView
 {
@@ -14,23 +16,33 @@ namespace WPFTreeView
     {
         #region Public Properties
         /// <summary>
-        /// The full path to an item
+        /// The type of an item
         /// </summary>
         public DirectoryItemType Type { get; set; }
+
+        public string ImageName => Type == DirectoryItemType.Drive ? "drive" : (Type == DirectoryItemType.File ? "file" : (IsExpanded ? "folder-open" : "folder-closed"));
 
         /// <summary>
         /// The full path to an item
         /// </summary>
         public string FullPath { get; set; }
 
+        /// <summary>
+        /// The name of the itwm
+        /// </summary>
         public string Name { get { return this.Type == DirectoryItemType.Drive ? this.FullPath : DirectoryStructure.GetFileFolderName(this.FullPath); } }
 
-        #endregion
+        /// <summary>
+        /// A list of all children contained inside this item
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Children { get; set; }
 
-        public ObservableCollection <DirectoryItemViewModel> Children { get; set; }
-        public bool CanExpland { get { return this.Type != DirectoryItemType.File; } }
+        /// <summary>
+        /// Indicates if this item is expanded
+        /// </summary>
+        public bool CanExpand { get { return this.Type != DirectoryItemType.File; } }
 
-        public bool isExpanded
+        public bool IsExpanded
         {
             get
             {
@@ -47,6 +59,31 @@ namespace WPFTreeView
             }
         }
 
+        #endregion
+
+        #region Public Commands
+        public ICommand ExpandCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public DirectoryItemViewModel(string fullPath, DirectoryItemType Type)
+        {
+            //Create command
+            this.ExpandCommand = new RelayCommand(Expand);
+
+            //Set path and type
+            this.FullPath = fullPath;
+            this.Type = Type;
+
+            //Setup the children as needed
+            this.ClearChildren();
+        }
+
+        #endregion
+
+
         #region Helper Method
         private void ClearChildren()
         {
@@ -60,10 +97,17 @@ namespace WPFTreeView
 
         #endregion
 
-        /// Expands the directory
+        /// <summary>
+        /// Expands this directory and finds all the children
+        /// </summary>
         private void Expand()
         {
+            if (this.Type == DirectoryItemType.File)
+                return;
 
+            var children = DirectoryStructure.GetDirectoryContents(this.FullPath);
+            this.Children = new ObservableCollection<DirectoryItemViewModel>(
+                            children.Select(content => new DirectoryItemViewModel(content.FullPath, content.Type)));
         }
     }
 }
